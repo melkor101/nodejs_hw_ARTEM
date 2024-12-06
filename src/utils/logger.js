7ed92478c7d85748d2df60f2f322eb2fa8f3ceb7
Artem Yatsyshyn <artem.yatsyshyn@litslink.com>
2024-12-06 22:34:43 +0200

    homework 01

---
+++ src/utils/logger.js
@@ -0,0 +1,11 @@
+module.exports = function getLogger(prefix) {
+
+  return {
+    log(message) {  
+      console.log(`${prefix}: ${message}`);
+    },
+    warn(message) {
+      console.error(`${prefix}: ${message}`);
+    }
+  };
+}
