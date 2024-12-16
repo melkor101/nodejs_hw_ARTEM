82ad41ba3bd55e32bc899c65fb829a34449e00ee
melkor101 <melkorkh@gmail.com>
2024-12-16 09:13:37 +0200

    fix log color

--- homework2/utils/logger.js
+++ homework2/utils/logger.js
@@ -8,16 +8,12 @@ const isColorsEnabled = config.has("COLORS_ENABLED") && config.get("COLORS_ENABL
 export default function getLogger(prefix) {
   return {
     log(message) {
-      const logName = "LOG:";
-      const showName =  isColorsEnabled ? colors.green(logName) :  logName;
-
-      console.log(showName, `${prefix}: ${message}`);
+      const showMessage =  isColorsEnabled ?  `${colors.green(prefix)}: ${message}` : `${prefix}: ${message}`;
+      console.log(showMessage);
     },
     warn(message) {
-      const logName = "WARN:";
-      const showName =  isColorsEnabled ? colors.yellow(logName) :  logName;
-
-      console.error(showName,`${prefix}: ${message}`);
+      const showMessage =  isColorsEnabled ?  `${colors.yellow(prefix)}: ${message}` : `${prefix}: ${message}`;
+      console.error(showMessage);
     }
   };
 }
